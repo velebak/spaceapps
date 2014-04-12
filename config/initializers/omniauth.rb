9545fc9f97fd678a2eae5569e9c9739576e6747a
Keith Veleba <keith@veleba.net>
2014-04-12 14:23:50 -0400

    commented out omniauth expiration time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  #provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
   provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :twitter, 'buIsBNlAZoewyZz6uCGTo9rEm', 'ImEtyc5HLBUhNRJPIgwm6EC1UR4H8dQxiUxHUSoHfgIHd1QAur'
+  provider :twitter, 'qqL0NOdekifztfdmI1R2k0ivF', 'OHcE8vHP6tqh5Cj4qwZrl8UcwkL2jndIOWjlekjwvw2OE8Xjc9'
 end
