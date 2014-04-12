31af7b8f01ad00fb15d1652574fd811c8b0144a0
Keith Veleba <keith@veleba.net>
2014-04-12 11:12:33 -0400

    work on user model and omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Spaceapps::Application.routes.draw do
+  get "landing/welcome"
+  root "landing#welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
