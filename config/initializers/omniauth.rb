3389dab03440a23785d97a33e6488b06e8759205
Keith Veleba <keith@veleba.net>
2014-04-12 13:19:39 -0400

    added twitter key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
   provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :twitter, 'YOUR-APP-ID', 'YOUR-SECRET'
+  provider :twitter, 'qqL0NOdekifztfdmI1R2k0ivF', 'OHcE8vHP6tqh5Cj4qwZrl8UcwkL2jndIOWjlekjwvw2OE8Xjc9'
 end
